9199e4e81f4319935c387d1fc990804f361bb41a
laboratorioesan <laboratorioesan@esan.edu.pe>
2025-09-20 09:47:44 -0500

    Initial commit

---
+++ app/src/main/java/dev/Rengatitos/appUE/presentation/auth/LoginScreen.kt
@@ -0,0 +1,60 @@
+package dev.Rengatitos.appUE.presentation.auth
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ){
+        Text("Iniciar Sesión", style = MaterialTheme.typography.titleLarge)
+
+        Text("correo electronico")
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email=it},
+            label = {Text("Correo Electronico")},
+            placeholder = {Text("Correo Electronico")},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password=it},
+            label = {Text("Contraseña")},
+            placeholder = {Text("Contraseña")},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            /*TODO*/
+        },modifier = Modifier.fillMaxWidth()
+            ) {
+            Text("Iniciar Sesión")
+
+        }
+
+
+    }
+}
+
+
